6ecf91ef8fd2ff78f86a1b6b0aaf84bef9a8b6e0
vierello <vierello@hotmail.com>
2016-06-13 23:40:05 -0400

    mostly done commit

---
+++ app/scripts/index.js
@@ -0,0 +1,134 @@
+
+// Hey Iron Yard Hackers! Enjoy!
+// Make sure to open your js consoles!
+
+//          __  _ ___ __  _
+//   __  __/ /_(_) (_) /_(_)__  _____
+//  / / / / __/ / / / __/ / _ \/ ___/
+// / /_/ / /_/ / / / /_/ /  __(__  )
+// \__,_/\__/_/_/_/\__/_/\___/____/
+
+// a simple "it" function for naming groups of expectations
+function it(description, contents){
+  console.log('\n\n"It '+ description + '"');
+  contents();
+}
+
+// A simple function for expecting values
+// Ex: expect(kepler.color).toBe('red'); // should return true
+function expect(target) {
+  return {
+    toBe: function(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation );
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation );
+        return false;
+      }
+    }
+  }
+}
+
+//                          __                  __
+//   _________  ____  _____/ /________  _______/ /_____  __________
+//  / ___/ __ \/ __ \/ ___/ __/ ___/ / / / ___/ __/ __ \/ ___/ ___/
+// / /__/ /_/ / / / (__  ) /_/ /  / /_/ / /__/ /_/ /_/ / /  (__  )
+// \___/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/  /____/
+//
+// Only add code to *THIS* section!
+
+function Dog(dogInfo){
+  this.status = "normal";
+  this.hungry = true;
+}
+
+function Human(personInfo){
+  this.pet = function(newDogInfo){
+    newDogInfo.status = 'happy';
+    newDogInfo.color = 'red';
+    newDogInfo.hungry = false;
+  }
+  this.feed = function(newDogInfo){
+    newDogInfo.hungry = false;
+  }
+}
+
+
+// console.log('2', dog.status);
+// }
+
+
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+var kepler = new Dog({
+  color: "red",
+  hungry: false
+});
+
+var moonshine = new Dog({
+  color: "blue-red"
+});
+
+var atticus = new Dog();
+
+
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+var dan = new Human();
+
+var faith = new Human({
+  cool: true
+});
+
+
+//                     __           __  __    _                             __
+//    ____ ___  ____ _/ /_____     / /_/ /_  (_)____   _      ______  _____/ /__
+//   / __ `__ \/ __ `/ //_/ _ \   / __/ __ \/ / ___/  | | /| / / __ \/ ___/ //_/
+//  / / / / / / /_/ / ,< /  __/  / /_/ / / / (__  )   | |/ |/ / /_/ / /  / ,<
+// /_/ /_/ /_/\__,_/_/|_|\___/   \__/_/ /_/_/____/    |__/|__/\____/_/  /_/|_|
+//
+// Don't edit this section. Instead make these tests pass by writing
+// constructors in the constructor section above ;D
+
+it("should make Kepler happy when Dan pets him", function(){
+  expect(kepler.status).toBe('normal');
+  dan.pet(kepler);
+  expect(kepler.status).toBe('happy');
+});
+
+it("should make Kepler red", function(){
+  expect(kepler.color).toBe('red');
+});
+
+it("should be make Moonshine hungry and Kepler not hungry", function(){
+  expect(moonshine.hungry).toBe(true);
+  expect(kepler.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function(){
+  faith.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+
+it("should not affect Atticus and Moonshine's owner properties when setting Dan as Kepler's owner ", function(){
+  kepler.owner = dan;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Faith cool and Dan not cool", function(){
+  kepler.owner = dan;
+  expect(faith.cool).toBe(true);
+  expect(dan.cool).toBe(false);
+});
